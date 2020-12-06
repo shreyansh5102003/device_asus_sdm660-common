f46d5adde4328907e09e08cb2b23233becd4b58d
sbwml <984419930@qq.com>
2020-12-06 15:01:17 +0800

    sdm660-common: Update portion IMS blobs from LA.UM.9.6.2.r1-02500-89xx.0
    - com.qualcomm.qti.uceservice 2.2
    - vendor.qti.imsrtpservice 3.0
    - camera.provider 2.6
    
    Change-Id: I69d3db2d45422231ad40480a6e7c01b40b82e2f8

--- sdm660.mk
+++ sdm660.mk
@@ -84,11 +84,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.4 \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.device@3.5:64 \
+    android.hardware.camera.provider@2.6:64 \
+    android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0:64
 
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
