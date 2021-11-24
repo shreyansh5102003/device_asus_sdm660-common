79d8eb62eb5172dc7ab1ac5a6f58526ca77aecb3
RONIN808 <shubhamshedage808@gmail.com>
2021-11-24 22:39:16 +0530

    sdm660-common: Enable AOSP surfaceflinger

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,6 +199,9 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
 
