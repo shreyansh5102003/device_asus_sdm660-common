fed7b279e4ff5ef8a33efc701483596117661630
Rαjαt Ҝαsђyαp <50959267+STRK-ND@users.noreply.github.com>
2022-01-03 13:14:10 +0530

    X00TD: define qcom hardware

--- sdm660.mk
+++ sdm660.mk
@@ -496,6 +496,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# Soong
+PRODUCT_BOARD_PLATFORM := sdm660
+PRODUCT_USES_QCOM_HARDWARE := true
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
